cf444910a534f94c23395272fe760759f99f487c
Matthieu BERTRAND <xplode-ccm@hotmail.fr>
2018-11-27 16:49:04 +0100

    100% passing tests for one-word Morse decoder

--- morse.py
+++ morse.py
@@ -2,5 +2,40 @@
 # pylint: disable=missing-docstring
 
 class Morse:
+    ALPHABET = {
+        '.-':   'A',
+        '-...': 'B',
+        '-.-.': 'C',
+        '-..':  'D',
+        '.':    'E',
+        '..-.': 'F',
+        '--.':  'G',
+        '....': 'H',
+        '..':   'I',
+        '.---': 'J',
+        '-.-':  'K',
+        '.-..': 'L',
+        '--':   'M',
+        '-.':   'N',
+        '---':  'O',
+        '.--.': 'P',
+        '--.-': 'Q',
+        '.-.':  'R',
+        '...':  'S',
+        '-':    'T',
+        '..-':  'U',
+        '...-': 'V',
+        '.--':  'W',
+        '-..-': 'X',
+        '-.--': 'Y',
+        '--..': 'Z'
+    }
+
     def decode(self, message):
-        pass # TODO: implement the behavior!
+        if message == '':
+            return ""
+        code = ""
+        letters = message.split(' ')
+        for letter in letters:
+            code = code + (self.ALPHABET[letter])
+        return code
